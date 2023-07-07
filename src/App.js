2babce427a900e8bf3baecc10d23bf37b9fede51
Macbook <macbook@MacBook-Pro-cua-Sinhdz.local>
2023-07-07 21:32:56 +0700

    add api photos, click loadmore page api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./asset/index.scss";
 // import Card from "./components/card/Card";
-import CardList from "./components/card/CardList";
-import CardTaiWind from "./components/card/CardTailWind";
+// import CardList from "./components/card/CardList";
+// import CardTaiWind from "./components/card/CardTailWind";
+import Photos from "./components/photo/Photos";
 function App() {
   return (
     <div className="App">
-      <CardList>
-        <CardTaiWind />
-      </CardList>
+      <Photos></Photos>
     </div>
   );
 }
---
+++ src/components/photo/Photos.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const GetRandomPhotos = (page) => {
+  return axios
+    .get(`https://picsum.photos/v2/list?page=${page}&limit=8`)
+    .then((response) => {
+      //   console.log(response);
+      return response.data;
+    })
+    .catch((error) => {});
+};
+
+// https://picsum.photos/v2/list
+// https://picsum.photos/v2/list?page=2&limit=100
+const Photos = () => {
+  const [randomPhotos, setRandomPhotos] = useState([]);
+  const [nextPage, setNextPage] = useState(1);
+
+  const handleLoadMorePhoto = async () => {
+    const images = await GetRandomPhotos(nextPage);
+    const newPhoto = [...randomPhotos, ...images];
+    setRandomPhotos(newPhoto);
+    //   setRandomPhotos(images);
+    setNextPage(nextPage + 1);
+  };
+  useEffect(() => {
+    handleLoadMorePhoto();
+  }, []);
+  return (
+    <div>
+      <div className="grid grid-cols-4 gap-5 p-5 ">
+        {randomPhotos.length > 0 &&
+          randomPhotos.map((item, index) => (
+            <div key={item.id} className="p-3 bg-white shadow-md rounded-lg">
+              <img
+                className="w-full h-full object-cover rounded-lg"
+                src={item.download_url}
+              ></img>
+            </div>
+          ))}
+      </div>
+      <div className="text-center">
+        <button
+          onClick={handleLoadMorePhoto}
+          className="inline-block px-8 py-4  bg-purple-600 text-white"
+        >
+          Load more
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Photos;
