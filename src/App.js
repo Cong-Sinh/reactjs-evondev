c41669256573df17bd87490f3275b05fc1651f25
Macbook <macbook@MacBook-Pro-cua-Sinhdz.local>
2023-07-12 13:09:11 +0700

    click chow component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import "./asset/index.scss";
-import Blog from "./components/Blog";
+// import Blog from "./components/Blog";
 // import DropDown from "./components/DropDown";
 // import TextareaAutoResize from "./components/TextareaAutoResize";
 // import StopWatch from "./components/StopWatch";
-// import HackerNews from "./components/news/HackerNews";
+import HackerNews from "./components/news/HackerNews";
 // import Timmer from "./components/Timmer";
 // import Header from "./components/Header";
 // import Counter from "./components/counter/Counter";
@@ -12,9 +13,11 @@ import Blog from "./components/Blog";
 // import CardTaiWind from "./components/card/CardTailWind";
 // import Photos from "./components/photo/Photos";
 function App() {
+  const [show, setShow] = useState(false);
   return (
     <div className="App">
-      <Blog></Blog>
+      <button onClick={() => setShow(!show)}>Toggle Butotn</button>
+      {show && <HackerNews></HackerNews>}
     </div>
   );
 }
